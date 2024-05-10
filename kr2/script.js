9362c403841abe9e92e48d37706960d4a801fc87
lliudmylla <159829505+lliudmylla@users.noreply.github.com>
2024-05-10 22:21:04 +0300

    Update script.js

--- kr2/script.js
+++ kr2/script.js
@@ -40,12 +40,12 @@ generateButton.addEventListener("click", function() {
     leftButton.addEventListener("click", function() {
         divPosition -= 20;
         div.style.position = "relative";
-        div.style.left = divPosition + "px";
+        div.style.top = divPosition + "px";
     });
 
     rightButton.addEventListener("click", function() {
         divPosition += 20;
         div.style.position = "relative";
-        div.style.left = divPosition + "px";
+        div.style.top = divPosition + "px";
     });
 });
