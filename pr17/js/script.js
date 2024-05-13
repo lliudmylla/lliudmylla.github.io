3496a24b39b77ba526218c5564474d946e372d9d
lliudmylla <159829505+lliudmylla@users.noreply.github.com>
2024-05-13 21:15:48 +0300

    Add files via upload

---
+++ pr17/js/script.js
@@ -0,0 +1,37 @@
+const slider = document.querySelector('.slider-wrapper');
+const prevButton = document.querySelector('.prev-btn');
+const nextButton = document.querySelector('.next-btn');
+let slideIndex = 0;
+
+function showSlides(index) {
+    const slides = document.querySelectorAll('.slide-card');
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+    slides.forEach((slide, i) => {
+        if (i === slideIndex) {
+            slide.style.display = "block";
+        } else {
+            slide.style.display = "none";
+        }
+    });
+}
+
+prevButton.addEventListener('click', () => {
+    slideIndex--;
+    showSlides(slideIndex);
+});
+
+nextButton.addEventListener('click', () => {
+    slideIndex++;
+    showSlides(slideIndex);
+});
+
+showSlides(slideIndex);
+
+setInterval(() => {
+    slideIndex++;
+    showSlides(slideIndex);
+}, 3000);
